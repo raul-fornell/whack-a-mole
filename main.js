829824aa4b49d35754c7d99b86227ac67004df08
Nacho Gómez García <nacho.gomez@sngular.com>
2021-11-25 11:49:32 +0100

    refactor: alternative using event delegate

--- main.js
+++ main.js
@@ -1,27 +1,17 @@
-const holes = Array.from(document.querySelector(".holes").children);
-const toggle = (item) => item.classList.toggle("occupied");
-[
-  [00, 01, 02, 03, 04],
-  [05, 06, 07, 08, 09],
-  [10, 11, 12, 13, 14],
-  [15, 16, 17, 18, 19],
-  [20, 21, 22, 23, 24],
-].forEach((row, rowIndex) => {
-  row.forEach((col, colIndex) => {
-    const hole = holes[col];
-    hole.addEventListener("click", () => {
-      toggle(hole);
-      colIndex > 0 && toggle(holes[col - 1]); // left
-      colIndex < 4 && toggle(holes[col + 1]); // right
-      rowIndex > 0 && toggle(holes[col - 5]); // top
-      rowIndex < 4 && toggle(holes[col + 5]); // bottom
-      const total = 25 - document.querySelectorAll(".occupied").length;
-      if (total === 0) {
-        document.querySelector(".counter").textContent = "You win";
-      } else {
-        document.querySelector(".counter").textContent = total;
-      }
-    });
-    Math.random() * 10 > 5 && toggle(hole);
-  });
+const container = document.querySelector('.holes');
+const holes = Array.from(container.children);
+const toggle = (item) => item.classList.toggle('occupied');
+
+container.addEventListener('click', (ev) => {
+  toggle(ev.target);
+  const holeIndex = Array.prototype.indexOf.call(holes, ev.target);
+  holeIndex % 5 !== 0 && toggle(holes[holeIndex - 1]); // left
+  (holeIndex - 4) % 5 !== 0 && toggle(holes[holeIndex + 1]); // right
+  holeIndex - 5 >= 0 && toggle(holes[holeIndex - 5]); // top
+  holeIndex + 5 < holes.length && toggle(holes[holeIndex + 5]); // bottom
+  const total = holes.length - document.querySelectorAll('.occupied').length;
+  document.querySelector('.counter').textContent =
+    total === 0 ? 'You win' : total;
 });
+
+holes.forEach((hole) => Math.random() > 0.5 && toggle(hole));
